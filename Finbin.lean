4c58e3bcdb36918cec4588641c095fb6295f83c6
roman3017 <rbacik@hotmail.com>
2026-06-07 20:02:20 -0700

    Fix imports

--- Finbin.lean
+++ Finbin.lean
@@ -1,7 +1,7 @@
+import Finbin.Bridge
 import Finbin.Embedding
-import Finbin.UnaryLinear
-import Finbin.QuarticDelta
 import Finbin.Obstruction.AlgebraicCore
 import Finbin.Obstruction.InversionGraph
 import Finbin.Obstruction.Quartic
-import Finbin.Bridge
+import Finbin.QuarticDelta
+import Finbin.UnaryLinear
